9b056a0be0ab00b0c0c88665c263e6ca5a7c79ca
Nikhil <nikhil.murade@nonstopio.com>
2025-03-26 16:31:01 +0530

    configuration for testing repo

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,10 +76,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db1',
-        'USER': 'user1',
-        'PASSWORD': '1234',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
